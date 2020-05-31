43032402246df88898e47acca22a9d6bc1cc5fbd
Vanessa Fiechtner <fiechtnervanessa@gmail.com>
2020-05-31 20:06:41 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -19,3 +19,20 @@ def swap_elements_from_to(array, index, destination_index)
   array[index], array[destination_index] = array[destination_index], array[index]
   return array
 end
+
+def reverse_array(integers)
+  new_array = integers.reverse
+  new_array
+end
+
+def kesha_maker(array)
+  kesha = []
+  array.each do |word|
+    word_array = word.split ""
+    word_array[2] = "$"
+    kesha << word_array.join
+  end
+  kesha
+end
+
+
